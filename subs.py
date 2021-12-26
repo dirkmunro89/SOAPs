dcfe0ead8cbd4e6a5bd0e30cc404bc0919eccc07
Dirk Munro <dirkmunro8@gmail.com>
2021-12-26 18:55:15 +0100

    subproblem flags changed

--- subs.py
+++ subs.py
@@ -9,17 +9,17 @@ from sub_qpl_exp import sub_qpl_exp
 #
 def subs(sub, n, m, x_k, x_d, x_l, x_u, g, dg, x_1, dg_1, mov, mov_rel, asy_fac, con_exp):
 #
-    if sub == 1:
+    if sub == 10:
         [x_p,x_d,dx_l,dx_u]=sub_mma(n,m,x_k,x_d,x_l,x_u,g,dg,mov,mov_rel,asy_fac)
-    elif sub == 2:
+    elif sub == 11:
         [x_p,x_d,dx_l,dx_u]=sub_mma_rel(n,m,x_k,x_d,x_l,x_u,g,dg,mov,mov_rel,asy_fac)
-    elif sub == 3:
+    elif sub == 20:
         [x_p,x_d,dx_l,dx_u]=sub_con(n,m,x_k,x_d,x_l,x_u,g,dg,mov,mov_rel)
-    elif sub == 4:
+    elif sub == 21:
         [x_p,x_d,dx_l,dx_u]=sub_con_exp(n,m,x_k,x_d,x_l,x_u,g,dg,mov,mov_rel,con_exp,x_1, dg_1)
-    elif sub == 5:
+    elif sub == 30:
         [x_p,x_d,dx_l,dx_u]=sub_qpq_exp(n,m,x_k,x_d,x_l,x_u,g,dg,mov,mov_rel,con_exp,x_1,dg_1)
-    elif sub == 6:
+    elif sub == 31:
         [x_p,x_d,dx_l,dx_u]=sub_qpl_exp(n,m,x_k,x_d,x_l,x_u,g,dg,mov,mov_rel,con_exp,x_1,dg_1)
     else:
         print('ERROR; subsolver not found')
